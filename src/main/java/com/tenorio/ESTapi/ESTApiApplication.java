9012476f6f3b843813b21ddbe44d6274019647e2
DanTenorio <juantenorio16@augustana.edu>
2023-04-14 11:17:24 -0500

    Added profiles

--- src/main/java/com/tenorio/ESTapi/ESTApiApplication.java
+++ src/main/java/com/tenorio/ESTapi/ESTApiApplication.java
@@ -2,15 +2,11 @@ package com.tenorio.ESTapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @SpringBootApplication
 public class ESTApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ESTApiApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(ESTApiApplication.class, args);}
 
 }
